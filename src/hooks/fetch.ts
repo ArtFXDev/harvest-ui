302050a8ed8435a556c00cf34ce3a17384575468
Joseph Henry <josephenry@protonmail.com>
2022-06-02 15:22:18 +0200

    fix: refresh use effect hook

--- src/hooks/fetch.ts
+++ src/hooks/fetch.ts
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { GetParams, GetResponse, GetRoutes } from "types/api";
 import { apiGet } from "utils/api";
-import { singleDepthEqual } from "utils/diff";
 
-import { usePrevious } from "./utils";
+import { useDeepCompareEffect } from "./utils";
 
 export interface FetchConfig {
   /** Interval in ms to refetch data */
@@ -18,15 +17,9 @@ export function useFetchData<R extends keyof GetRoutes>(
   config: FetchConfig = {},
   params: Partial<GetParams<R>> = {}
 ) {
-  const previousParams = usePrevious(params);
   const [data, setData] = useState<GetResponse<R>>();
 
-  useEffect(() => {
-    // Make sure previous params are not equal
-    // It was creating an infinite loop since comparing an object was triggering useEffect every time
-    // See: https://stackoverflow.com/questions/54095994/react-useeffect-comparing-objects
-    if (previousParams && singleDepthEqual(params, previousParams)) return;
-
+  useDeepCompareEffect(() => {
     const fetchData = async () => {
       const result = await apiGet<R>(route, params);
       setData(result);
@@ -51,7 +44,7 @@ export function useFetchData<R extends keyof GetRoutes>(
     if (interval) {
       return () => clearInterval(interval as NodeJS.Timeout);
     }
-  }, [config.interval, params, previousParams, route]);
+  }, [config.interval, params, route]);
 
   return data;
 }
--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,15 +1,24 @@
-import { useEffect, useRef } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { EffectCallback, useEffect, useRef } from "react";
+import { singleDepthEqual } from "utils/diff";
 
-/**
- * Generic hook for storing a value
- * // See: https://stackoverflow.com/questions/54095994/react-useeffect-comparing-objects
- */
-export function usePrevious<T>(value: T) {
-  const ref = useRef<T>();
+function useDeepCompareMemoize(value: Record<string, unknown> | undefined) {
+  const ref = useRef<unknown>();
 
-  useEffect(() => {
+  if (ref.current && !singleDepthEqual(value as any, ref.current as any)) {
     ref.current = value;
-  });
+  }
 
   return ref.current;
+}
+
+// Make sure previous params are not equal
+// It was creating an infinite loop since comparing an object was triggering useEffect every time
+// See: https://stackoverflow.com/questions/54095994/react-useeffect-comparing-objects
+export function useDeepCompareEffect(
+  callback: EffectCallback,
+  dependencies: unknown[]
+) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(callback, dependencies.map(useDeepCompareMemoize as any));
 }
